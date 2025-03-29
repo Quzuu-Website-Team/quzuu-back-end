2b7fd53e538550c40d6ac4501a693b10627be43a
imdipo <imammuhammaddiponegoro@gmail.com>
2025-03-29 23:37:12 +0700

    Rename Public to IsPublic in Event struct

--- models/database_orm_model.go
+++ models/database_orm_model.go
@@ -74,7 +74,7 @@ type Events struct {
 	StartEvent time.Time `json:"start_event"`
 	EndEvent   time.Time `json:"end_event"`
 	SID        string    `json:"sid"`
-	Public     string    `json:"public"`
+	IsPublic     bool    `json:"is_public"`
 }
 
 type Announcement struct {
