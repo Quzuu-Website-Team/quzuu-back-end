1ba9ba7fa457c35950f0c4a64169f648f7d690fd
abdanhafidz <abdan.hafidz@gmail.com>
2025-05-07 21:52:44 +0700

    remove: need jwt for email verification

--- router/email_route copy.go
+++ router/email_route copy.go
@@ -3,13 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	EmailController "godp.abdanhafidz.com/controller/email"
-	"godp.abdanhafidz.com/middleware"
 )
 
 func EmailRoute(router *gin.Engine) {
 	routerGroup := router.Group("/api/v1/email")
 	{
-		routerGroup.POST("/verify", middleware.AuthUser, EmailController.Verify)
-		routerGroup.POST("/create-verification", middleware.AuthUser, EmailController.CreateVerification)
+		routerGroup.POST("/verify", EmailController.Verify)
+		routerGroup.POST("/create-verification", EmailController.CreateVerification)
 	}
 }
