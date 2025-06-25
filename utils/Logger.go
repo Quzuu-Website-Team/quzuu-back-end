7aa72577cc458d92b32b915ff2ce8c7e134d940b
ABDAN HAFIDZ <71253590+abdanhafidz@users.noreply.github.com>
2025-06-25 21:37:27 +0700

    Update Logger.go

--- utils/Logger.go
+++ utils/Logger.go
@@ -3,20 +3,13 @@ package utils
 import (
 	"fmt"
 	"log"
-	"os"
-
 	"godp.abdanhafidz.com/config"
 )
 
 func LogError(errorLogged error) {
 	fmt.Println("There is an error!")
-	file, err := os.OpenFile(config.LOG_PATH+"/error_log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("Error Log :", errorLogged)
-	log.SetOutput(file)
-
-	log.Println("Error Log :", errorLogged)
 }
